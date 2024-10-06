791af9de0c70f854ccead519e9886f85f583ab01
chris2500 <chrisvasquez985@gmail.com>
2024-10-06 01:37:35 -0600

    Edicion de servicios para solicitudes de amistad

--- HiveGameServer/HiveGameService/Contracts/IFriendRequestManager.cs
+++ HiveGameServer/HiveGameService/Contracts/IFriendRequestManager.cs
@@ -11,11 +11,30 @@ namespace HiveGameService.Contracts
     [ServiceContract]
     public interface IFriendRequestManager
     {
+
+        [OperationContract]
+        int CreateFriendRequest(Profile playerOne, Profile playerTwo);
+        [OperationContract]
+        List<Profile> GetFriendRequests(Profile player);
+        [OperationContract]
+        int AcceptFriendRequest(Profile playerOne, Profile playerTwo);
+        [OperationContract]
+        int DeclineFriendRequest(Profile playerOne, Profile playerTwo);
+        [OperationContract]
+        int VerifyFriendRequestRegistered(Profile playerOne, Profile playerTwo);
+        
     }
 
     [DataContract]
     public class FriendShip
     {
-
+        [DataMember]
+        public int idFriendRequest {  get; set; }
+        [DataMember]
+        public int idPlayerOne {  get; set; }
+        [DataMember]
+        public int idPlayerTwo { get; set; }
+        [DataMember]
+        public string state { get; set; }
     }
 }
---
+++ HiveGameServer/HiveGameService/Services/FriendshipService.cs
@@ -0,0 +1,27 @@
+﻿using HiveGameService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiveGameService.Services
+{
+    internal class FriendshipService : IFriendshipManager
+    {
+        public int DeleteFriend(Profile removingPlayer, Profile friendToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Profile> GetAllFriends(Profile player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Profile GetFriendByUsername(Profile player, string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
