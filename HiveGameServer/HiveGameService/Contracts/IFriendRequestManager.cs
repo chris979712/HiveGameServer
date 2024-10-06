50a2ec87d312e87f07487fa7f48641557337e6ad
chris2500 <chrisvasquez985@gmail.com>
2024-10-05 23:39:55 -0600

    Declaración de servicios en el app.Config

---
+++ HiveGameServer/HiveGameService/Contracts/IFriendRequestManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace HiveGameService.Contracts
+{
+    [ServiceContract]
+    public interface IFriendRequestManager
+    {
+    }
+
+    [DataContract]
+    public class FriendShip
+    {
+
+    }
+}
