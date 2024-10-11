4bfe4ad36ce82053dc9d99437f7dc35e26ee7b98
Chris_VZ19 <chrisvasquez985@gmail.com>
2024-10-11 12:04:37 -0600

    Si

---
+++ HiveGameServer/DataBaseManager/Utilities/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiveGameService.Utilities
+{
+    public static class Constants
+    {
+        public const int ERROR_OPERATION = -1;
+
+        public const int SUCCES_OPERATION = 1;
+
+        public const int NO_DATA_MATCHES = 0;
+
+        public const int DATA_MATCHES = 1;
+
+        public const string DEFAULT_IMAGE_PLAYER = " ";
+
+        public const string DEFAULT_IMAGE_GUEST = " ";
+
+    }
+}
