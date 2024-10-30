7d5fb81d896f08649a9fbc39208e87aebe1d7992
Chris_VZ19 <chrisvasquez985@gmail.com>
2024-10-29 20:22:23 -0600

    Arreglo y mejora en la sesión de usuario y amigos

--- HiveGameServer/HiveGameService/Services/UserSessionService.cs
+++ HiveGameServer/HiveGameService/Services/UserSessionService.cs
@@ -12,25 +12,26 @@ namespace HiveGameService.Services
 {
     public partial class HiveGameService : IUserSessionManager
     {
-        private static readonly List<string> usersConnected = new List<string>();
-        public int ConnectToGame(string username)
+        private static readonly List<UserSession> usersConnected = new List<UserSession>();
+        public int ConnectToGame(UserSession user)
         {
             int resultConnection = Constants.ERROR_OPERATION;
-            if (!usersConnected.Contains(username))
+            if (!usersConnected.Contains(user))
             {
-                usersConnected.Add(username);
+                usersConnected.Add(user);
+                UpdateFriendsListOfConectedFriends(user);
                 resultConnection = Constants.SUCCES_OPERATION;
             }
             return resultConnection;
         }
 
-        public int Disconnect(string username)
+        public int Disconnect(UserSession user)
         {
             int resultDisconnection = Constants.ERROR_OPERATION;
-            if (usersConnected.Contains(username))
+            if (usersConnected.Contains(user))
             {
-                usersConnected.Remove(username);
-                UpdateFriendsListOfConectedFriends(username);
+                usersConnected.Remove(user);
+                UpdateFriendsListOfConectedFriends(user);
                 resultDisconnection = Constants.SUCCES_OPERATION;
             }
             else
@@ -40,29 +41,27 @@ namespace HiveGameService.Services
             return resultDisconnection;
         }
 
-        public bool VerifyConnectivity(string username)
+        public bool VerifyConnectivity(UserSession user)
         {
             bool resultVerification = false;
-            if (usersConnected.Contains(username))
+            if (usersConnected.Contains(user))
             {
                 resultVerification = true;
             }
             return resultVerification;
         }
 
-        public void UpdateFriendsListOfConectedFriends(string username)
+        public void UpdateFriendsListOfConectedFriends(UserSession user)
         {
             LoggerManager logger = new LoggerManager(this.GetType());
-            Profile userProfile = GetUserProfileByUsername(username);
-            List<string> connnectedFriends = ObtainFriendsList(userProfile.idAccesAccount);
+            List<UserSession> connnectedFriends = ObtainFriendsList(user.idAccount);
             for(int connectedFriendsListIndex = 0; connectedFriendsListIndex < connnectedFriends.Count; connectedFriendsListIndex++)
             {
                 try
                 {
                     if (friendsManagerCallbacks.ContainsKey(connnectedFriends[connectedFriendsListIndex]))
                     {
-                        Profile friendProfile = GetUserProfileByUsername(connnectedFriends[connectedFriendsListIndex]);
-                        friendsManagerCallbacks[connnectedFriends[connectedFriendsListIndex]].ObtainConnectedFriends(ObtainFriendsList(friendProfile.idAccesAccount));
+                        friendsManagerCallbacks[connnectedFriends[connectedFriendsListIndex]].ObtainConnectedFriends(ObtainFriendsList(connnectedFriends[connectedFriendsListIndex].idAccount));
                     }
                 }catch(CommunicationException communicationException)
                 {
