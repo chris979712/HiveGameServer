77e4dd14d2720b7624e8a0daf028a6c7dac290a9
Juan-Cumplido <zs22020936@estudiantes.uv.mx>
2024-11-28 16:48:13 -0600

    se hizo la documentación de la API en todos las interfaces de los contratos

--- HiveGameServer/HiveGameService/Contracts/IGameManager.cs
+++ HiveGameServer/HiveGameService/Contracts/IGameManager.cs
@@ -10,21 +10,54 @@ namespace HiveGameService.Contracts
     [ServiceContract(CallbackContract = typeof(IGameManagerCallback))]
     interface IGameManager
     {
+
+        /// <summary>
+        /// Connects an user to the gameboard of the match
+        /// </summary>
+        /// <param name="session">Contains the data of the user who is going to join the gameboard</param>
+        /// <param name="codeMatch">Contains the code of the match</param>
         [OperationContract(IsOneWay = true)]
         void ConnectToGameBoard(UserSession session, string codeMatch);
 
+        /// <summary>
+        /// Broadcast the finish of the match with the corresponding result
+        /// </summary>
+        /// <param name="codeMatch">Contains the code of the match to send the finish notification</param>
+        /// <param name="winner">Contains the name of the winner</param>
         [OperationContract(IsOneWay = true)]
         void FinishOfTheMatch(string codeMatch, string winner);
 
+        /// <summary>
+        /// Broadcast the corresponding moved piece on the board
+        /// </summary>
+        /// <param name="piece">Contains the piece that was moved</param>
+        /// <param name="session">Conatins the data of the user who moved the piece</param>
+        /// <param name="codeMatch">Contains the code of the match</param>
         [OperationContract(IsOneWay = true)]
         void MovePiece(GamePice piece, UserSession session, string codeMatch);
 
+        /// <summary>
+        /// Set the initial and later turns
+        /// </summary>
+        /// <param name="session">Contains the data of the actual player with the turns</param>
+        /// <param name="codeMatch">Contains the code of the match</param>
         [OperationContract(IsOneWay = true)]
         void SetTurns(UserSession session, string codeMatch);
 
+        /// <summary>
+        /// Let a user leave the match that is in progress
+        /// </summary>
+        /// <param name="session">Contains the data of the user who is going to leave</param>
+        /// <param name="codeMatch">Contains the code of the match</param>
         [OperationContract(IsOneWay = true)]
         void LeaveTheGame(UserSession session, string codeMatch);
 
+        /// <summary>
+        /// Let a user leave the match when it's finished
+        /// </summary>
+        /// <param name="codeMatch">Contains the code of the match</param>
+        /// <param name="session">Contains the data of the user who is going to leave</param>
+        /// <returns></returns>
         [OperationContract]
         int LeaveMatchFinished(string codeMatch, UserSession session);
     }
@@ -32,21 +65,45 @@ namespace HiveGameService.Contracts
     [ServiceContract]
     public interface IGameManagerCallback
     {
+        /// <summary>
+        /// Charge the information of the user who joins the match
+        /// </summary>
+        /// <param name="side">Contains the side where the player is going to be</param>
         [OperationContract]
         void ChargePlayerGameBoard(PlayerSide side);
 
+        /// <summary>
+        /// Receives the corresponding moved piece in the board
+        /// </summary>
+        /// <param name="piece">Contains the piece that was moved</param>
         [OperationContract]
         void ReceivePieceMoved(GamePice piece);
 
+        /// <summary>
+        /// Receive the corresponding an actual turns
+        /// </summary>
+        /// <param name="isTurn">Contains if it's player turns or not</param>
         [OperationContract]
         void ReceiveTurns(bool isTurn);
 
+        /// <summary>
+        /// Receive the list of players connected to the gameboard
+        /// </summary>
+        /// <param name="userSession">Listo of players who are in the game</param>
         [OperationContract]
         void ReceivePlayersToMatch(List<UserSession> userSession);
 
+        /// <summary>
+        /// Receive the notification that a player left the game
+        /// </summary>
+        /// <param name="doPlayerLeftTheGame">Contains the bool with the result if it left</param>
         [OperationContract]
         void ReceivePlayerHasLeftNotification(bool doPlayerLeftTheGame);
 
+        /// <summary>
+        /// Receive the notification with the result of the winner
+        /// </summary>
+        /// <param name="winner">Contains the name of the winner</param>
         [OperationContract]
         void ReceiveFinalMatchResult(string winner);
     }
@@ -55,7 +112,7 @@ namespace HiveGameService.Contracts
     public class PlayerSide
     {
         [DataMember]
-        public string username {  get; set; }
+        public string username { get; set; }
 
         [DataMember]
         public bool playerOne { get; set; }
@@ -74,13 +131,13 @@ namespace HiveGameService.Contracts
         public string imagePath { get; set; }
 
         [DataMember]
-        public int numberOfPiece {  get; set; }
+        public int numberOfPiece { get; set; }
 
         [DataMember]
-        public Point position {  get; set; }
+        public Point position { get; set; }
 
         [DataMember]
-        public string playerName {  get; set; }
+        public string playerName { get; set; }
     }
 
     [DataContract]
--- HiveGameServer/HiveGameService/Contracts/ILeaderBoardManager.cs
+++ HiveGameServer/HiveGameService/Contracts/ILeaderBoardManager.cs
@@ -9,18 +9,42 @@ namespace HiveGameService.Contracts
     [ServiceContract]
     public interface ILeaderBoardManager
     {
+        /// <summary>
+        /// Update the win result leaderboard of a player
+        /// </summary>
+        /// <param name="idAccounr">Contains the id of the player to update</param>
+        /// <returns>Returns 1 if the modification was successfull</returns>
         [OperationContract]
         int UpdateWinnerResultToPlayerLeaderBoard(int idAccounr);
 
+        /// <summary>
+        /// Update the lose result leaderboard of a player
+        /// </summary>
+        /// <param name="idAccount">Contains the id of the player to update</param>
+        /// <returns>Returns 1 if the modification was successfull</returns>
         [OperationContract]
         int UpdateLoserResultToPlayerLeaderBoard(int idAccount);
 
+        /// <summary>
+        /// Update the drwaw result leaderboard of a player
+        /// </summary>
+        /// <param name="idAccount">Contains the id of the player to update</param>
+        /// <returns>Returns 1 if the modification was successfull</returns>
         [OperationContract]
         int UpdateDrawResultToPlayerLeaderBoard(int idAccount);
 
+        /// <summary>
+        /// Get the personal leaderboard of a player
+        /// </summary>
+        /// <param name="idAccount">Contains the id of the player to get the leaderboard</param>
+        /// <returns>Returns the leaderboard obtained</returns>
         [OperationContract]
         LeaderBoardPlayer GetPersonalLeaderBoard(int idAccount);
 
+        /// <summary>
+        /// Get all the leaderboards from the database
+        /// </summary>
+        /// <returns>List of all the leaderboard obtained</returns>
         [OperationContract]
         List<LeaderBoardPlayer> GetAllPlayersLeaderboards();
     }
@@ -30,11 +54,11 @@ namespace HiveGameService.Contracts
     {
         [DataMember]
         public int totalMatches;
-        [DataMember] 
+        [DataMember]
         public int totalDrawMatches;
         [DataMember]
         public int totalWonMatches;
-        [DataMember] 
+        [DataMember]
         public int totalLostMatches;
         [DataMember]
         public string username;
